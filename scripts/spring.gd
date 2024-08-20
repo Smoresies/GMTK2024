acdfb17059c999ec9051f592fd0b9b8a784517b5
Smoresies <gavin.britton@gmail.com>
2024-08-19 18:08:12 -0700

    More soda can work

--- scripts/spring.gd
+++ scripts/spring.gd
@@ -8,7 +8,9 @@ func _on_body_entered(body):
 		#print(body)
 	if body.get_meta("Player"):
 		# var vectorTo: Vector2 = (position.direction_to(body.position) + transform.y).normalized()
+		print(transform.y)
 		body.velocity = -transform.y * SPRINGFORCE
+		print(body.velocity)
 		spring_animator.play("bounce")
 		await spring_animator.animation_finished
 		spring_animator.play("default")
